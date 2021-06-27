ed184087594bce1624f3e05b5839312d6c8e49e9
Mario Rossatti Junior <rossattijuniordeveloper@gmail.com>
2021-06-27 17:05:44 -0400

    criamos a classe Usuario

---
+++ class/Usuario.php
@@ -0,0 +1,85 @@
+<?php 
+class Usuario{
+
+	private $idusuario;
+	private $deslogin;
+	private $dessenha;
+	private $dtcadastro;
+
+
+//---------------------------------------------	
+//
+// metdodos customizados
+//
+//---------------------------------------------	
+	public function loadById($id){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",array(
+			":ID"=>$id
+		) );
+		if(count( $results)>0 ){
+			$row = $results[0];
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro( new DateTime($row['dtcadastro']));
+		}
+	}	
+//---------------------------------------------
+	public function __toString(){
+		if($this->idusuario>0){
+			return json_encode(array(
+				"idusuario"=>$this->getIdusuario(),
+				"deslogin"=>$this->getDeslogin(),
+				"dessenha"=>$this->getDessenha(),
+				"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+			));		
+		}
+		else
+		{
+			return json_encode(array("Usuario não encontrado!"));
+		}			
+	}
+//---------------------------------------------
+
+
+//---------------------------------------------	
+//
+// metdodos GETTERS SETTERS
+//
+//---------------------------------------------	
+	public function getIdusuario (){
+		return $this->idusuario ;
+	}
+	public function setIdusuario($value){
+		$this->idusuario = $value;
+	}
+//---------------------------------------------
+	public function getDeslogin (){
+		return $this->deslogin ;
+	}
+	public function setDeslogin ($value){
+		$this->deslogin = $value;
+	}
+//---------------------------------------------
+	public function getDessenha (){
+		return $this->dessenha ;
+	}
+	public function setDessenha($value){
+		$this->dessenha = $value;
+	}
+//---------------------------------------------
+	public function getDtcadastro(){
+		return $this->dtcadastro ;
+	}
+	public function setDtcadastro($value){
+		$this->dtcadastro = $value;
+	}
+//---------------------------------------------
+
+}
+
+
+
+
+ ?>
