a914f29bacb978c1548caf8988e39caa57e05521
Mario Rossatti Junior <rossattijuniordeveloper@gmail.com>
2021-06-29 15:48:42 -0400

    aula 66 criamos metodos de Listas

--- class/Usuario.php
+++ class/Usuario.php
@@ -26,12 +26,52 @@ class Usuario{
 		}
 	}	
 //---------------------------------------------
+	public static function getList(){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+
+//---------------------------------------------
+	public static function search($login){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", 
+			array(':SEARCH'=>"%".$login."%"
+		));
+
+	}
+//---------------------------------------------
+	public function login($login,$password){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD " ,array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		) );
+
+		if(count( $results)>0 ){
+			$row = $results[0];
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro( new DateTime($row['dtcadastro']));
+		}
+		else
+		{
+			throw new Exception("Usuário não encontrado! O Login e\ou Senha estão ERRADOS");
+			
+		}	
+
+
+	}
+//---------------------------------------------
 	public function __toString(){
 		if($this->idusuario>0){
 			return json_encode(array(
-				"idusuario"=>$this->getIdusuario(),
-				"deslogin"=>$this->getDeslogin(),
-				"dessenha"=>$this->getDessenha(),
+				"idusuario"=> $this->getIdusuario(),
+				"deslogin"=>  $this->getDeslogin(),
+				"dessenha"=>  $this->getDessenha(),
 				"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 			));		
 		}
@@ -47,7 +87,8 @@ class Usuario{
 //
 // metdodos GETTERS SETTERS
 //
-//---------------------------------------------	
+//---------------------------------------------
+			// Idusuario
 	public function getIdusuario (){
 		return $this->idusuario ;
 	}
@@ -55,6 +96,7 @@ class Usuario{
 		$this->idusuario = $value;
 	}
 //---------------------------------------------
+			//Deslogin
 	public function getDeslogin (){
 		return $this->deslogin ;
 	}
@@ -62,6 +104,7 @@ class Usuario{
 		$this->deslogin = $value;
 	}
 //---------------------------------------------
+			// Dessenha
 	public function getDessenha (){
 		return $this->dessenha ;
 	}
@@ -69,6 +112,7 @@ class Usuario{
 		$this->dessenha = $value;
 	}
 //---------------------------------------------
+			//Dtcadastro
 	public function getDtcadastro(){
 		return $this->dtcadastro ;
 	}
--- index.php
+++ index.php
@@ -1,16 +1,37 @@
 <?php
 
 require_once("config.php");
-$root = new Usuario();
-$root->loadById(2);
 
-echo $root;
 
 /*
+// aula 65 exemplo de carregar o usuario por id
+$root = new Usuario();
+$root->loadById(1);
+echo $root;
+
+*/
+
+/*
+ //echo "<h1>  Aula 66 - DAO LIST => exemplo de carregar uma lista de usuarios </h1>";
 $sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
+
+/*
+// Aula 66 - DAO LIST => exemplo de carregar uma lista de usuarios usando um metodo de uma classe sem preciar instanciar o Objeto.
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+// Aula 66 - DAO LIST => exemplo de buscar uma lista de usuarios usando um metodo de uma classe , usando o campo deslogin
+$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+// Aula 66 - DAO LIST => carega o usuario usando login e a senha
+$usuario = new Usuario();
+$usuario->login("jose","123456");
+echo $usuario;
 
 
 
